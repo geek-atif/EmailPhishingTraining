55d9ea95a3b09c4fc3231be25d12eddd7686599c
Atif Qamar <atifqamar29@gmail.com>
2022-10-12 19:34:06 +0530

    update

--- lib/ui/native_code/platform_channel_handler.dart
+++ lib/ui/native_code/platform_channel_handler.dart
@@ -11,7 +11,7 @@ class PlatformChannelHandler {
   static const platform = MethodChannel('sampleChannel');*/
 
   static const platform = const MethodChannel('sampleChannel');
-  Future<String> getTotp() async {
+  String getTotp() async {
     var totp = "";
     try {
       totp = await platform.invokeMethod('test');
